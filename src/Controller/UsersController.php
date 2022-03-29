015be22344e77d726127e8de0a220e091eba7a68
FadhiMessaoudi <fad.messaoudi@hotmail.fr>
2022-03-29 15:29:47 +0200

    fix users

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,13 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Annonces;
 use App\Form\AnnoncesType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class UsersController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
+    #[Route('/users', name: 'users_')]
     public function index(): Response
     {
         return $this->render('users/index.html.twig', [
@@ -19,11 +24,24 @@ class UsersController extends AbstractController
     }
 
     #[Route('/users/annonces/ajout', name: 'users_annonces_ajout')]
-    public function ajoutAnnonce(): Response
+    public function ajoutAnnonce(HttpFoundationRequest $request, ManagerRegistry $doctrine): Response
     {
         $annonce = new Annonces;
 
         $form = $this->createForm(AnnoncesType::class, $annonce);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonce->setUsers($this->getUser());
+            $annonce->setActive(false);
+
+            $em = $doctrine->getManager();
+            $em->persist($annonce);
+            $em->flush();
+
+            return $this->redirectToRoute('users_');
+        }
 
         return $this->render('users/annonces/ajout.html.twig', [
             'form' => $form->createView(),
